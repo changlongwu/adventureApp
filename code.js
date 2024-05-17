2d219771604480780fae18aff5702245bef24974
changlongwu <changlong_wu@berkeley.edu>
2024-05-17 13:07:56 -0700

    add all files

---
+++ code.js
@@ -0,0 +1,12 @@
+onEvent("buttonPolarBear", "click", function( ) {
+	setScreen("screenPolarBear");
+});
+onEvent("buttonSnowMobile", "click", function( ) {
+  setScreen("screenSnowMobile");
+});
+onEvent("buttonTryAgain", "click", function( ) {
+  setScreen("screenStart");
+});
+onEvent("buttonPlayAgain", "click", function( ) {
+  setScreen("screenStart");
+});
